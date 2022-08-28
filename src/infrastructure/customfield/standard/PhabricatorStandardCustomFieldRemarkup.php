81f5fd4f271019ced198c91114a263ea8fa47425
Abijeet <abijeetpatro@gmail.com>
2022-08-28 10:35:24 +0530

    More strlen project creation fixes

--- src/infrastructure/customfield/standard/PhabricatorStandardCustomFieldRemarkup.php
+++ src/infrastructure/customfield/standard/PhabricatorStandardCustomFieldRemarkup.php
@@ -30,7 +30,7 @@ final class PhabricatorStandardCustomFieldRemarkup
   public function renderPropertyViewValue(array $handles) {
     $value = $this->getFieldValue();
 
-    if (!strlen($value)) {
+    if (!strlen($value ?? '')) {
       return null;
     }
 
